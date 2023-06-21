55916463e448b169f77268c54976caee256b4b50
sandhya <sandhya@sandhyas-mbp.lan>
2023-06-21 00:06:20 -0700

    commit..

---
+++ src/main/java/mavenproject/git2.java
@@ -0,0 +1,16 @@
+package mavenproject;
+
+public class git2 {
+
+	public static void main(String[] args) {
+		int i=6;
+		if (i==5) {
+			System.out.println(true);
+		}
+		else {
+			System.out.println(false);
+		}
+
+	}
+
+}
